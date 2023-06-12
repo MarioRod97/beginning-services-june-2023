63c19545c8bc61e27d12a7ad7bb9baf0390e89c7
Mario Rodriguez <mario_rodriguez_36@progressive.com>
2023-06-12 16:31:53 -0400

    AFternoon push

---
+++ backend/BusinessApi/BusinessApi.IntegrationTests/Clock/ResourceTests.cs
@@ -0,0 +1,57 @@
+﻿using Alba;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+namespace BusinessApi.IntegrationTests.Clock;
+
+public class ResourceTests
+{
+
+    [Fact]
+    public async Task GivesMeA200()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/clock");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+
+    [Fact]
+    public async Task DuringOpenHours()
+    {
+        var expected = new GetClockResponse(true, null);
+
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/clock");
+        });
+
+        var responseMessage = response.ReadAsJson<GetClockResponse>();
+
+        Assert.Equal(expected, responseMessage);
+    }
+    
+    [Fact]
+    public async Task DuringClosedHours()
+    {
+        // This will fail on saturdays, sundays, before 9 and after 5
+        var expected = new GetClockResponse(false, null);
+
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/clock");
+        });
+
+        var responseMessage = response.ReadAsJson<GetClockResponse>();
+
+        //.Assert.Equal(expected, responseMessage);
+        Assert.False(responseMessage.IsOpen);
+    }
+}
--- backend/BusinessApi/BusinessApi/Program.cs
+++ backend/BusinessApi/BusinessApi/Program.cs
@@ -1,11 +1,20 @@
+using BusinessApi.Adapters;
+using BusinessApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Transient, Scoped, and Singleton - tomorrow morning we will learn how to choose these.
+builder.Services.AddScoped<BusinessClock>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
 
+// everything above here is configuring the "guts" of our API
 var app = builder.Build();
+// everything AFTER here is setting up the Request/Response pipeline.
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -14,29 +23,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
+app.MapGet("/clock", ([FromServices] BusinessClock businessClock) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var response = businessClock.GetClockResponse();
+    return Results.Ok(response);
+});
+// Start the Web Server (Kestrel)
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
