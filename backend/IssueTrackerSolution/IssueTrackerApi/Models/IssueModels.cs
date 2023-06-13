4fa8a4653dffa1597faa728359882a94c95d36f4
Mario Rodriguez <mario_rodriguez_36@progressive.com>
2023-06-13 16:34:10 -0400

    End of day push!!!!

---
+++ backend/IssueTrackerSolution/IssueTrackerApi/Models/IssueModels.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTrackerApi.Models;
+
+public record IssueCreateRequest
+{
+    [Required, MaxLength(20)]
+    public string From { get; set; } = string.Empty;
+    [Required]
+    public string Issue { get; set; } = string.Empty;
+}
+
+public record IssueCreatedResponse
+{
+    public Guid Id { get; set; } = Guid.Empty;
+    public string From { get; set; } = string.Empty;
+    public string Issue { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? ClosedAt { get; set; }
+}
